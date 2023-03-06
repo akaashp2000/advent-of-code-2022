228a44605fec4401497799f43882c745f0dd7603
akaashp2000 <akaashp2000@gmail.com>
2023-03-06 22:56:43 +0000

    added folder and trying to import in day 12 NB

---
+++ modules_folder/mapgraph.py
@@ -0,0 +1,44 @@
+class MapGraph:
+    def __init__(self, vertices, edges, inverted=False):
+        self.vertices = vertices
+        self.edges = edges
+
+        if inverted:
+            self.edges = {}
+
+    def shortest_distance(self, start, end):
+        visited = [start]
+
+        layers = {0: [start]}
+        layer = 0
+        while end not in visited:
+
+            layers[layer+1] = []
+
+            for vertex in layers[layer]:
+                for neighbour in [edge[1] for edge in self.edges[vertex]]:
+                    if neighbour not in visited:
+                        layers[layer + 1].append(neighbour)
+                        visited.append(neighbour)
+
+            layer += 1
+
+        return layer
+
+    def shortest_distance_multiple(self, start, ends):
+        visited = [start]
+        distances = {end: None for end in ends}
+        
+        layers = {0: [start]}
+        layer = 0
+        while True:
+            layers[layer+1] = []
+
+            for vertex in layers[layer]:
+                for neighbour in [edge[1] for edge in self.edges[vertex]]:
+                    if neighbour not in visited:
+                        layers[layer + 1].append(neighbour)
+                        visited.append(neighbour)
+                        if neighbour in ends:
+                            return layer + 1
+            layer += 1
